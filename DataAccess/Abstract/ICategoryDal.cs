e670262322b38a63dc04e3e81c57bbc692985be6
Mustafa Ateş <mstfaatesss@gmail.com>
2021-02-03 22:45:55 +0300

    EntityFramework implemented

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal
+    public interface ICategoryDal : IEntityRepository<Category>
     {
-      
+
     }
 }
--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using Entities.Abstract;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //Generic constraint
+    //Class : reference tip
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesen olabilir.
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll();
-        T Get();
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int categoryId);
     }
 }
