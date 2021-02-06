9bc8e5c421e2180d8f7143fe990622f5a2a115cb
Mustafa Ateş <mstfaatesss@gmail.com>
2021-02-06 23:24:00 +0300

    Various Changes

--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -1,12 +1,12 @@
-﻿using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using Core.DataAccess;
+using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public interface IOrderDal : IEntityRepository<Order>
     {
 
     }
