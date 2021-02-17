60535525061c1d02cecd112cb515c9bdffa75071
Mustafa Ateş <mstfaatesss@gmail.com>
2021-02-17 22:10:50 +0300

    Validation Rules added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -22,6 +22,10 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
+            if (product.UnitPrice <= 0)
+            {
+                return new ErrorResult(Messages.UnitPriceInvalid);
+            }
             if (product.ProductName.Length < 2)
             {
                 //magic strings
