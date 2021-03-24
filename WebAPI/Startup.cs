74481e0ccc7b0571b5d770c606e67f448c2ef930
Mustafa Ateş <mstfaatesss@gmail.com>
2021-03-24 22:45:34 +0300

    minor changes

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -29,6 +29,8 @@ namespace WebAPI
             //AOP
             services.AddControllers();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -58,6 +60,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
